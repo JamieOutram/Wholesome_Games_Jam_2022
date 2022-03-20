904c54777655103b20db148452f6d68df2f640c1
Luke Woolley <35926332+lukegwoolley@users.noreply.github.com>
2022-03-20 10:25:31 +0000

    simple version

---
+++ Assets/Scripts/cheese.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cheese : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.tag == "Player")
+        {
+            Destroy(gameObject);
+            //SceneManager.LoadScene("Village");
+        }
+    }
+}
